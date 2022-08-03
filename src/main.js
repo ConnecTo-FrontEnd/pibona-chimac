e700ef4e1866640a38993bf7aed7a584a97e2572
loco9939 <kls9939@naver.com>
2022-08-03 16:56:30 +0900

    refactor: Edit commit
    변수로 true, false 해봤습니다.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 function Fibonacci(n) {
   let fibonacci = [1, 1];
-  while (true) {
+  let state = true;
+  while (state) {
     let value =
       fibonacci[fibonacci.length - 1] + fibonacci[fibonacci.length - 2];
-    if (value > n) break;
+    if (value > n) {
+      state = false;
+      break;
+    }
     fibonacci.push(value);
   }
+  console.log(fibonacci);
 }
 
+Fibonacci(6);
